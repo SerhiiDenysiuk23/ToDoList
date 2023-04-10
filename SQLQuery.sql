3848b69ca3e74ec07222dcc58cc79c23e1bc337b
Сергій Денисюк <kn202_dsm@student.ztu.edu.ua>
2023-04-10 16:38:58 +0300

    Fixed sql query

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -2,7 +2,7 @@
 	Id INT PRIMARY KEY NOT NULL IDENTITY,
 	Title NVARCHAR(50) NOT NULL,
 	[Description] NVARCHAR(MAX) NULL,
-	CategoryId INT FOREIGN KEY REFERENCES Category(Id) NULL,
+	CategoryId INT FOREIGN KEY REFERENCES Category(Id) ON DELETE SET NULL,
 	DueDate DATE NULL,
 	[Status] NVARCHAR(15) CHECK ([Status] IN ('In progress', 'Completed')) DEFAULT 'In progress' NOT NULL
 )
@@ -11,7 +11,3 @@ CREATE TABLE Category(
 	Id INT PRIMARY KEY NOT NULL IDENTITY,
 	[Name] NVARCHAR(15) NOT NULL
 )
-
-select * from ToDo
-
-SELECT t.*, c.* FROM ToDo t LEFT JOIN Category c ON t.CategoryId = c.Id
